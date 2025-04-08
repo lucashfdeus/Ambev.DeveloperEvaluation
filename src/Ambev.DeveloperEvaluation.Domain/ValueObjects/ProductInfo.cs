514e5ef740f81265cf0118fd22f31e09cf96f7c3
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 23:05:57 -0300

    feat(Ambev.DeveloperEvaluation.Domain.ValueObjects): Define Value Object ProductInfo com validação.

---
+++ src/Ambev.DeveloperEvaluation.Domain/ValueObjects/ProductInfo.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects
+{
+    public record ProductInfo
+    {
+        public string Name { get; }
+        public string Code { get; }
+
+        public ProductInfo(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public ValidationResultDetail Validate()
+        {
+            var validator = new ProductInfoValidator();
+            var result = validator.Validate(this);
+            return new ValidationResultDetail
+            {
+                IsValid = result.IsValid,
+                Errors = result.Errors.Select(e => (ValidationErrorDetail)e)
+            };
+        }
+    }
+}

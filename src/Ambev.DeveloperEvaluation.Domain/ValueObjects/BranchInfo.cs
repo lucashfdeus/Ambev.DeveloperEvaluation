f577a21595d960052c33fb5e16022608aa2baa6d
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 23:04:11 -0300

    feat(Ambev.DeveloperEvaluation.Domain.ValueObjects): Define Value Object BranchInfo com validação.

---
+++ src/Ambev.DeveloperEvaluation.Domain/ValueObjects/BranchInfo.cs
@@ -0,0 +1,28 @@
+﻿using Ambev.DeveloperEvaluation.Common.Validation;
+using Ambev.DeveloperEvaluation.Domain.Validation;
+
+namespace Ambev.DeveloperEvaluation.Domain.ValueObjects
+{
+    public record BranchInfo
+    {
+        public string Name { get; }
+        public string Code { get; }
+
+        public BranchInfo(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+
+        public ValidationResultDetail Validate()
+        {
+            var validator = new BranchInfoValidator();
+            var result = validator.Validate(this);
+            return new ValidationResultDetail
+            {
+                IsValid = result.IsValid,
+                Errors = result.Errors.Select(e => (ValidationErrorDetail)e)
+            };
+        }
+    }
+}

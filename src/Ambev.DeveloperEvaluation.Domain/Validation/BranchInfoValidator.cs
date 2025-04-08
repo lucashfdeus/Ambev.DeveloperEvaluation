aad45fe1eeb4ba3b3f9cdbed176ba62e09a29d8d
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 22:54:50 -0300

    feat(Ambev.DeveloperEvaluation.Domain.Validation): Implementa validação para Value Objects de Filial.

---
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/BranchInfoValidator.cs
@@ -0,0 +1,18 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class BranchInfoValidator : AbstractValidator<BranchInfo>
+    {
+        public BranchInfoValidator()
+        {
+            RuleFor(b => b.Code)
+           .NotEmpty().WithMessage("Branch code is required.");
+
+            RuleFor(b => b.Name)
+                .NotEmpty().WithMessage("Branch name is required.")
+                .MaximumLength(100).WithMessage("Branch name cannot exceed 100 characters.");
+        }
+    }
+}

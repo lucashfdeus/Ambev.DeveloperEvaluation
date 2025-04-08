6d4cf8aa070304fe4ec97054b757b0b3d574a51e
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 22:55:35 -0300

    feat(Ambev.DeveloperEvaluation.Domain.Validation): Implementa validação para Value Objects de SaleItem.

---
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/SaleItemValidator.cs
@@ -0,0 +1,27 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Entities;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class SaleItemValidator : AbstractValidator<SaleItem>
+    {
+        public SaleItemValidator()
+        {
+            RuleFor(x => x.ProductId)
+           .NotEmpty().WithMessage("ProductId must not be empty.");
+
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+
+            RuleFor(x => x.UnitPrice)
+                .GreaterThan(0).WithMessage("UnitPrice must be greater than zero.");
+
+            RuleFor(x => x.Discount)
+                .GreaterThanOrEqualTo(0).WithMessage("Discount cannot be negative.");
+
+            RuleFor(x => x)
+                .Must(item => item.Discount <= item.UnitPrice * item.Quantity)
+                .WithMessage("Discount cannot be greater than the total value of the item.");
+        }
+    }
+}

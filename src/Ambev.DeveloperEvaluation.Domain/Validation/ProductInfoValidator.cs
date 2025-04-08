ffb1c4478b77382aef734830d18cd945b02405d4
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 23:02:14 -0300

    feat(Ambev.DeveloperEvaluation.Domain.Validation): Implementa validação para Value Objects de ProductInfo.

---
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/ProductInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class ProductInfoValidator : AbstractValidator<ProductInfo>
+    {
+        public ProductInfoValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Product name is required.");
+
+            RuleFor(c => c.Code)
+                .NotEmpty().WithMessage("Product code is required.");
+        }
+    }
+}

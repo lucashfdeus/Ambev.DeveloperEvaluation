5bf53fc4e90951b8d32202300f7fef98c22be614
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 22:53:57 -0300

    feat(Ambev.DeveloperEvaluation.Domain.Enums): Adicionando enumerador para definir os status de uma venda.

---
+++ src/Ambev.DeveloperEvaluation.Domain/Enums/SaleStatus.cs
@@ -0,0 +1,23 @@
+﻿namespace Ambev.DeveloperEvaluation.Domain.Enums
+{
+    /// <summary>
+    /// Represents the current status of a sale.
+    /// </summary>
+    public enum SaleStatus
+    {
+        /// <summary>
+        /// The sale is open and can be modified.
+        /// </summary>
+        Pending = 1,
+
+        /// <summary>
+        /// The sale has been finalized and can no longer be changed.
+        /// </summary>
+        Finalized = 2,
+
+        /// <summary>
+        /// The sale has been canceled and is no longer valid.
+        /// </summary>
+        Canceled = 3
+    }
+}

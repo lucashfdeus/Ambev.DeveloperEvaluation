f95b1210d10444812dae13845a7b56516a032d37
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-07 22:55:12 -0300

    feat(Ambev.DeveloperEvaluation.Domain.Validation): Implementa validação para Value Objects de CustomerInfo.

---
+++ src/Ambev.DeveloperEvaluation.Domain/Validation/CustomerInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿using Ambev.DeveloperEvaluation.Domain.ValueObjects;
+using FluentValidation;
+
+namespace Ambev.DeveloperEvaluation.Domain.Validation
+{
+    public class CustomerInfoValidator : AbstractValidator<CustomerInfo>
+    {
+        public CustomerInfoValidator()
+        {
+            RuleFor(c => c.Name)
+                .NotEmpty().WithMessage("Customer name is required.");
+
+            RuleFor(c => c.Document)
+                .NotEmpty().WithMessage("Customer document is required.");
+        }
+    }
+}

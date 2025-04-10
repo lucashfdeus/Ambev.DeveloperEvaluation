e7a926ea87ed38440e0f496b2b557adb0b56f617
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-10 00:04:45 -0300

    feat(Ambev.DeveloperEvaluation.Application.Sales.Events): Adicionando classe implementação do SaleCreatedEventHandler. Evento puplicado em console apenas para teste.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/Events/SaleCreatedEventHandler.cs
@@ -0,0 +1,15 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Events.Sale;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.Events
+{
+    public class SaleCreatedEventHandler : INotificationHandler<SaleCreatedEvent>
+    {
+        public Task Handle(SaleCreatedEvent notification, CancellationToken cancellationToken)
+        {
+            Console.WriteLine($"[Evento] Venda criada: {notification.SaleId}, Cliente: {notification.CustomerId}");
+
+            return Task.CompletedTask;
+        }
+    }
+}

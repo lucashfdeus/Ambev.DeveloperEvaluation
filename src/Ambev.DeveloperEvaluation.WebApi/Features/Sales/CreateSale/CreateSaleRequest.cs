5b67b640cbf010920bc8b5c6c52691926d8f4f2d
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-10 00:12:17 -0300

    feat(Ambev.DeveloperEvaluation.WebApi.Features.Sales): Adicionando novo endpont para criação da Sale, e implementação das classes de requests e responses.

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequest.cs
@@ -0,0 +1,28 @@
+﻿namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    public class CreateSaleRequest
+    {
+        public string SaleNumber { get; set; } = string.Empty;
+
+        public Guid CustomerId { get; set; } = Guid.Empty;
+
+        public string CustomerName { get; set; } = string.Empty;
+
+        public Guid BranchId { get; set; } = Guid.Empty;
+
+        public string BranchName { get; set; } = string.Empty;
+
+        public DateTime SaleDate { get; set; } = DateTime.UtcNow;
+
+        public List<CreateSaleItemRequest> Items { get; set; } = [];
+    }
+
+    public class CreateSaleItemRequest
+    {
+        public Guid ProductId { get; set; } = Guid.Empty;
+        public string ProductName { get; set; } = string.Empty;
+
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.WebApi/Features/Sales/CreateSale/CreateSaleRequestValidator.cs
@@ -0,0 +1,42 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.CreateSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Users.CreateUser;
+using FluentValidation;
+using static Ambev.DeveloperEvaluation.Application.Sales.CreateSale.CreateSaleCommand;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Features.Sales.CreateSale
+{
+    public class CreateSaleRequestValidator : AbstractValidator<CreateSaleRequest>
+    {
+        public CreateSaleRequestValidator()
+        {
+            RuleFor(sale => sale.SaleNumber).NotEmpty();
+            RuleFor(sale => sale.CustomerId).NotEmpty();
+            RuleFor(sale => sale.CustomerName).NotEmpty();
+            RuleFor(sale => sale.BranchId).NotEmpty();
+            RuleFor(sale => sale.BranchName).NotEmpty();
+            RuleFor(sale => sale.SaleDate).NotEmpty().LessThanOrEqualTo(DateTime.Now);
+
+            RuleForEach(sale => sale.Items)
+                .SetValidator(new CreateSaleItemRequestValidator());
+
+        }
+    }
+
+    public class CreateSaleItemRequestValidator : AbstractValidator<CreateSaleItemRequest>
+    {
+        public CreateSaleItemRequestValidator()
+        {
+            RuleFor(item => item.ProductId)
+                .NotEmpty().WithMessage("Product ID is required.");
+
+            RuleFor(item => item.ProductName)
+                .NotEmpty().WithMessage("Product name is required.");
+
+            RuleFor(item => item.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+
+            RuleFor(item => item.UnitPrice)
+                .GreaterThan(0).WithMessage("Unit price must be greater than zero.");
+        }
+    }
+}

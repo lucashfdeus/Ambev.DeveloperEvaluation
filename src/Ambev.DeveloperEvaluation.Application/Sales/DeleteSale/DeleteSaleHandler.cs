2e5ff4895399e681db7b0d73df00cb645ff48ede
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-13 22:14:38 -0300

    feat(Ambev.DeveloperEvaluation.Application.Sales.DeleteSale): Adicionando command para Delete Sale.

---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleHandler.cs
@@ -0,0 +1,35 @@
+﻿using Ambev.DeveloperEvaluation.Domain.Repositories;
+using AutoMapper;
+using FluentValidation;
+using MediatR;
+
+namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
+{
+    public class DeleteSaleHandler : IRequestHandler<DeleteSaleCommand, DeleteSaleResponse>
+    {
+        private readonly ISaleRepository _saleRepository;
+        private readonly IMapper _mapper;
+
+        public DeleteSaleHandler(ISaleRepository saleRepository, IMapper mapper)
+        {
+            _saleRepository = saleRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<DeleteSaleResponse> Handle(DeleteSaleCommand request, CancellationToken cancellationToken)
+        {
+            var validator = new DeleteSaleValidator();
+            var validationResult = await validator.ValidateAsync(request, cancellationToken);
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors);
+
+            var success = await _saleRepository.DeleteAsync(request.Id, cancellationToken);
+
+            if (!success)
+                throw new KeyNotFoundException($"User with ID {request.Id} not found");
+
+            return new DeleteSaleResponse { Success = true };
+        }
+    }
+}
---
+++ src/Ambev.DeveloperEvaluation.Application/Sales/DeleteSale/DeleteSaleResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace Ambev.DeveloperEvaluation.Application.Sales.DeleteSale
+{
+    /// <summary>
+    /// Response model for DeleteSale operation
+    /// </summary>
+    public class DeleteSaleResponse
+    {
+        public bool Success { get; set; }
+    }
+}

1d4eb2233a38a4577ce71fc54a6fb85aab2b4d4f
lucashfdeus <lucas.hfdeus@gmail.com>
2025-04-13 21:49:22 -0300

    feat(Ambev.DeveloperEvaluation.WebApi.Mappings.Sale):Adicionando novos mappings para getSale.

---
+++ src/Ambev.DeveloperEvaluation.WebApi/Mappings/Sale/GetSaleRequestProfile.cs
@@ -0,0 +1,21 @@
+﻿using Ambev.DeveloperEvaluation.Application.Sales.GetSale;
+using Ambev.DeveloperEvaluation.WebApi.Features.Sales.GetSale;
+using AutoMapper;
+
+namespace Ambev.DeveloperEvaluation.WebApi.Mappings.Sale
+{
+    public class GetSaleRequestProfile : Profile
+    {
+        public GetSaleRequestProfile()
+        {
+            CreateMap<Guid, GetSaleCommand>()
+                .ConstructUsing(id => new GetSaleCommand(id));
+
+            CreateMap<GetSaleResult, GetSaleResponse>()
+                .ForMember(dest => dest.Status, opt => opt.MapFrom(src => src.Status.ToString()));
+
+            CreateMap<GetSaleItemResult, GetSaleItemResponse>()
+                .ForMember(dest => dest.IsCancelled, opt => opt.MapFrom(src => src.IsCancelled));
+        }
+    }
+}
